480e8cf68a7141fd11b8dc064a4ab88318ccfbbd
Eddie Fisher <eddi.fisher@gmail.com>
2015-03-12 18:11:45 +0300

    added new methods to announce
    result, cinema, city, time_zone, schedule_date

--- lib/kinomax_parser/announce.rb
+++ lib/kinomax_parser/announce.rb
@@ -1,17 +1,56 @@
 require "uri"
+require "date"
 
 module KinomaxParser
   class Announce
     attr_accessor :doc, :url
 
     def initialize(attributes = {})
-      params = { cinema: attributes[:cinema], date: attributes[:date] }.compact
+      date = Date.parse(attributes[:date]).to_s unless attributes[:date].nil?
+      params = { cinema: attributes[:cinema], date: date }.compact
       @url = "http://kinomax.ru/export/schedule?#{URI.encode_www_form(params)}"
       @doc = Nokogiri::XML(File.open('./spec/fixtures/announce.xml')) if attributes[:test] == true
+      doc
+    end
+
+    def result
+      {
+        result: @doc.at_css('result').text,
+        message: @doc.at_css('message').text
+      }
+    end
+
+    def cinema
+      cinema = @doc.at_css('cinema')
+      {
+        id: cinema.attribute('id').text.to_i,
+        token: cinema.attribute('token').text,
+        name: cinema.text
+      }
+    end
+
+    def city
+      city = @doc.at_css('city')
+      {
+        id: city.attribute('id').text.to_i,
+        name: city.text
+      }
+    end
+
+    def time_zone
+      zone = @doc.at_css('cityDatetime')
+      {
+        time_zone: zone.attribute('timezone').text,
+        request_time: zone.text
+      }
+    end
+
+    def schedule_date
+      @doc.at_css('scheduleForDate').text
     end
 
     def movies
-      doc.xpath('//movies//movie').map { |movie| KinomaxParser::Movie.new(movie) }
+      @doc.xpath('//movies//movie').map { |movie| KinomaxParser::Movie.new(movie) }
     end
 
     private
--- spec/announce_spec.rb
+++ spec/announce_spec.rb
@@ -2,7 +2,23 @@ require 'spec_helper'
 
 describe KinomaxParser::Announce do
   let(:default) { KinomaxParser::Announce.new test: true }
+  # let(:default) { KinomaxParser::Announce.new cinema: :vladimir, date: '2015-03-13' }
+  let(:data) {
+    {
+      result: { result: 'ok', message: '' },
+      url: 'http://kinomax.ru/export/schedule?',
+      cinema: { id: 4, token: 'vladimir', name: 'Киномакс-Буревестник Владимир' },
+      city: { id: 3, name: 'Владимир' },
+      time_zone: { time_zone: "Europe/Moscow", request_time: '2015-03-12 16:48:00' },
+      schedule_date: '2015-03-12'
+    }
+  }
 
-  it { expect(default.url).to eq('http://kinomax.ru/export/schedule?') }
+  it { expect(default.result).to eq(data[:result]) }
+  it { expect(default.url).to eq(data[:url]) }
   it { expect(default.movies.count).to eq(6) }
+  it { expect(default.cinema).to eq(data[:cinema]) }
+  it { expect(default.city).to eq(data[:city]) }
+  it { expect(default.time_zone).to eq(data[:time_zone]) }
+  it { expect(default.schedule_date).to eq(data[:schedule_date]) }
 end
