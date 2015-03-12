1a1e38af59ac9794a75c8f1dded0bbad955b14e0
Eddie Fisher <eddi.fisher@gmail.com>
2015-03-12 16:54:47 +0300

    updated test with real data

--- spec/announce_spec.rb
+++ spec/announce_spec.rb
@@ -4,5 +4,5 @@ describe KinomaxParser::Announce do
   let(:default) { KinomaxParser::Announce.new test: true }
 
   it { expect(default.url).to eq('http://kinomax.ru/export/schedule?') }
-  it { expect(default.movies.count).to eq(1) }
+  it { expect(default.movies.count).to eq(6) }
 end
